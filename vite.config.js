51faed02f5f69b7ab5974ec5ebe4922f022d99ec
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2024-07-02 17:53:31 -0500

    Update vite.config.js
    Base added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://https://danisebastian9.github.io/vite-react'
 })
